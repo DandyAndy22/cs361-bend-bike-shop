951325fe02e6e08f822e7bfd03103f1be127fa47
Andras Mihaly <andrasm96@gmail.com>
2020-02-12 13:18:50 -0800

    Refactor all the classes

--- rental.rb
+++ rental.rb
@@ -2,12 +2,24 @@ class Rental
 
   attr_reader :bike
 
-  def initialize(bike)
+  def initialize(bike, luggage)
     @bike = bike
   end
 
-  def base_price
-    self.bike.price + self.bike.luggage.items.count * 10
+  def total_price
+    bike_price + luggage_price
+  end
+
+  def bike_price
+    self.bike.price
+  end
+
+  def luggage_price
+    self.luggage.weight * 10
+  end
+
+  def weight
+    self.bike.weight + self.luggage.weight
   end
 
 end
